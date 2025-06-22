ddd833857a4ae31a3e96559e7663956b26fff1ee
Sushmita Aich <138333831+sushmitaaich@users.noreply.github.com>
2025-06-22 08:22:45 +0530

    Add files via upload

---
+++ Week 1 - Engineering Concepts/Design Patterns and Principles/Exercise 2 - FactoryMethodPatternExample/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        DocumentFactory factory = new PdfFactory();
+        Document doc = factory.createDocument();
+        doc.open();
+    }
+}
