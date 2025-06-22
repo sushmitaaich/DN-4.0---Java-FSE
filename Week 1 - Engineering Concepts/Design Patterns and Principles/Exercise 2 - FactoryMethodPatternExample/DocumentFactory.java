9d9745a8535709738358da615a340c733be447de
Sushmita Aich <138333831+sushmitaaich@users.noreply.github.com>
2025-06-22 08:21:53 +0530

    Create DocumentFactory.java

---
+++ Week 1 - Engineering Concepts/Design Patterns and Principles/Exercise 2 - FactoryMethodPatternExample/DocumentFactory.java
@@ -0,0 +1,21 @@
+abstract class DocumentFactory {
+    public abstract Document createDocument();
+}
+
+class WordFactory extends DocumentFactory {
+    public Document createDocument() {
+        return new WordDocument();
+    }
+}
+
+class PdfFactory extends DocumentFactory {
+    public Document createDocument() {
+        return new PdfDocument();
+    }
+}
+
+class ExcelFactory extends DocumentFactory {
+    public Document createDocument() {
+        return new ExcelDocument();
+    }
+}
