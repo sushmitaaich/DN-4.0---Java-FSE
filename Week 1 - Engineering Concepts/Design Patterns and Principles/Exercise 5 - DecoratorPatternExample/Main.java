ea26c96ab9d07d3b205b2786b07ce2e3019f4b34
Sushmita Aich <138333831+sushmitaaich@users.noreply.github.com>
2025-06-22 21:12:55 +0530

    Update Main.java

--- Week 1 - Engineering Concepts/Design Patterns and Principles/Exercise 5 - DecoratorPatternExample/Main.java
+++ Week 1 - Engineering Concepts/Design Patterns and Principles/Exercise 5 - DecoratorPatternExample/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        PaymentProcessor processor = new StripeAdapter(new StripeGateway());
-        processor.processPayment(250.00);
+        Notifier notifier = new SMSNotifier(new EmailNotifier());
+        notifier.send("Your order has been shipped.");
     }
 }
