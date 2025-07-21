7f720fff789d97711ed5e0ebf3d62ae1ff493f12
Sushmita Aich <aichsushmita08@gmail.com>
2025-07-21 15:07:39 +0530

    Completed Microservices

---
+++ WEEK - 5/Employee_id_001/Microservices/account/src/main/java/com/cognizant/account/AccountApplication.java
@@ -0,0 +1,16 @@
+package com.cognizant.account;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccountApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccountApplication.class, args);
+	}
+
+}
+
+
+//E:\JAVA FSE\Employee_id_001\Microservices\account\src\main\java\com\cognizant\account\AccountApplication.java
