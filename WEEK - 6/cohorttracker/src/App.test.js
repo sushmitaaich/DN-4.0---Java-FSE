d0c793a54eb012c38c7ef4efb22f81621c575813
Sushmita Aich <138333831+sushmitaaich@users.noreply.github.com>
2025-08-05 22:58:01 +0530

    Delete WEEK - 6 directory

--- WEEK - 6/cohorttracker/src/App.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test('renders Cognizant Cohort Tracker title', () => {
-  render(<App />);
-  const titleElement = screen.getByText(/Cognizant Cohort Tracker/i);
-  expect(titleElement).toBeInTheDocument();
-});
